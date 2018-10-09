ffecf3579177e68fde82dd8c9feb7f9c8bda7fa3
kurumuz <kurumuz1@gmail.com>
2018-10-09 19:02:54 +0300

    removed r cmd

--- botstart.py
+++ botstart.py
@@ -30,14 +30,16 @@ async def on_ready():
 async def on_message(message: Message):
     if message.author == bot.user:
         return
+    if not kayit_check(message):
+        register(message)
     if await image_download(message):
         return
-    global dcheck
-    if kayit_check(message):
-        file = await aiofiles.open("db/" + message.author.name + "/acheck", "r")
-        acheck = await file.read()
-        if acheck != "NULL":
-            return await get_answer_image(message)
+    global dcheck       
+       
+    file = await aiofiles.open("db/" + message.author.name + "/acheck", "r")
+    acheck = await file.read()
+    if acheck != "NULL":
+        return await get_answer_image(message)
     if dcheck == 1: #TODO: kişiye özel olmalı
         return await selectDers(message)
     try:
@@ -59,46 +61,46 @@ async def is_image(message: Message):
 
 async def image_download(message: Message):
 
-    if kayit_check(message) == True:
-        file = open("db/" + message.author.name + "/ders", "r")
-        dersargumani = file.read()
-        file = await aiofiles.open("db/" + message.author.name + "/acheck", "r")
-        acheck = await file.read()
-        if dersargumani != "NULL" and acheck == "NULL":
+    file = open("db/" + message.author.name + "/ders", "r")
+    dersargumani = file.read()
+    file = await aiofiles.open("db/" + message.author.name + "/acheck", "r")
+    acheck = await file.read()
+    if dersargumani != "NULL" and acheck == "NULL":
 
-            if await is_image(message):
-                i = 0
-                resimcount = len(message.attachments)
-                print(str(resimcount) + " soru var")
-                if not os.path.exists("db/" + message.author.name + "/Çözülmemiş/" + dersargumani + "/"):
-                    os.makedirs("db/" + message.author.name + "/Çözülmemiş/" + dersargumani + "/")
-                    file = await aiofiles.open("db/" + message.author.name + "/Çözülmemiş/" + dersargumani + "/db", "a+")
-                    await file.write("1")
+        if await is_image(message):
+            i = 0
+            resimcount = len(message.attachments)
+            print(str(resimcount) + " soru var")
+            if not os.path.exists("db/" + message.author.name + "/Çözülmemiş/" + dersargumani + "/"):
+                os.makedirs("db/" + message.author.name + "/Çözülmemiş/" + dersargumani + "/")
+                file = await aiofiles.open("db/" + message.author.name + "/Çözülmemiş/" + dersargumani + "/db", "a+")
+                await file.write("1")
                 
-                while (i != resimcount):
-                    file = await aiofiles.open("db/" + message.author.name + "/Çözülmemiş/" + dersargumani + "/db", "r+")
-                    sorucount = await file.readline()
-                    print(sorucount)
-                    url = message.attachments[i]['url']
-                    print(url)
-                    path = "db/" + message.author.name + "/Çözülmemiş/" + dersargumani + "/" + dersargumani + sorucount + ".jpg"
-                    await download_file(url, path)
-                    file = await aiofiles.open("db/" + message.author.name + "/Çözülmemiş/" + dersargumani + "/db", "w+")
-                    if os.path.isfile(path):
-                        await file.write(str(int(sorucount) + 1))
-                        #TODO: Liste
-                        file.close()
-                    else:
-                        await bot.send_message(message.channel, "İndirme sırasında hata!" + message.author.mention)
-                    await bot.send_message(message.channel, "Sorunuz " + dersargumani + sorucount + " adı ile kayıt edildi." + message.author.mention)
-                    i = i + 1
-                return         
+            while (i != resimcount):
+                file = await aiofiles.open("db/" + message.author.name + "/Çözülmemiş/" + dersargumani + "/db", "r+")
+                sorucount = await file.readline()
+                print(sorucount)
+                url = message.attachments[i]['url']
+                print(url)
+                path = "db/" + message.author.name + "/Çözülmemiş/" + dersargumani + "/" + dersargumani + sorucount + ".jpg"
+                await download_file(url, path)
+                file = await aiofiles.open("db/" + message.author.name + "/Çözülmemiş/" + dersargumani + "/db", "w+")
+                if os.path.isfile(path):
+                    await file.write(str(int(sorucount) + 1))
+                    #TODO: Liste
+                    file.close()
+                else:
+                    await bot.send_message(message.channel, "İndirme sırasında hata!" + message.author.mention)
+                await bot.send_message(message.channel, "Sorunuz " + dersargumani + sorucount + " adı ile kayıt edildi." + message.author.mention)
+                i = i + 1
+            return         
 
-            if len(message.attachments) < 1:
-                print("resim yok")
-                return
+        if len(message.attachments) < 1:
+            print("resim yok")
+            return
 
 async def download_file(url, path):
+    
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as resp:
             if resp.status == 200:
@@ -115,28 +117,21 @@ def kayit_check(message: Message):
         return False
     return
 
-@bot.command(pass_context=True)
-async def r(ctx):
+def register(message: Message):
 
-    if not os.path.exists("db/" + ctx.message.author.name):
-        os.makedirs("db/" + ctx.message.author.name)
-        file = open("db/" + ctx.message.author.name + "/otodb", "w+")
-        file = open("db/" + ctx.message.author.name + "/manudb", "w+")
-        file.write("1")
-        file = open("db/" + ctx.message.author.name + "/ders", "w+")
-        file.write("NULL")
-        file = open("db/" + ctx.message.author.name + "/acheck", "w+")
-        file.write("NULL")
-        file = open("db/" + ctx.message.author.name + "/sudo", "w+")
-        file.write("0")
-        os.makedirs("db/" + ctx.message.author.name + "/Çözülmemiş/")
-        os.makedirs("db/" + ctx.message.author.name + "/Çözülmüş/")
-
-        file.close()
-        return await bot.send_message(ctx.message.channel, content = ctx.message.author.mention + " veritabanına kaydedildin!")
-    else:
-        return await bot.send_message(ctx.message.channel, content = ctx.message.author.mention + " zaten kayıtlısın.")
-    return
+    os.makedirs("db/" + message.author.name)
+    file = open("db/" + message.author.name + "/otodb", "w+")
+    file = open("db/" + message.author.name + "/manudb", "w+")
+    file.write("1")
+    file = open("db/" + message.author.name + "/ders", "w+")
+    file.write("NULL")
+    file = open("db/" + message.author.name + "/acheck", "w+")
+    file.write("NULL")
+    file = open("db/" + message.author.name + "/sudo", "w+")
+    file.write("0")
+    os.makedirs("db/" + message.author.name + "/Çözülmemiş/")
+    os.makedirs("db/" + message.author.name + "/Çözülmüş/")
+    file.close()
 
 def init():
     if not os.path.exists("db"):
@@ -266,44 +261,42 @@ async def get_answer_image(message: Message):
 
 @bot.command(pass_context=True) #soruya cevap vermek veya çözülmüş işaretlemek için.
 async def a(ctx, *args):
-    if kayit_check(ctx.message) == True:
-        file = await aiofiles.open("db/" + ctx.message.author.name + "/ders", "r")
-        dersargumani = await file.read()
-        path = "db/" + ctx.message.author.name + "/Çözülmemiş/" + dersargumani
-        if dersargumani != "NULL" and len(args) == 1 and os.path.exists(path):
-            if os.path.isfile(path + "/" + dersargumani + args[0] + ".jpg"):              
-                await bot.send_file(ctx.message.channel, path + "/" + dersargumani + args[0] + ".jpg")
-                await bot.send_message(ctx.message.channel, "bu soru için cevap gönderin, iptal için iptal yazın." + ctx.message.author.mention)
-                acheck = dersargumani + args[0] + ".jpg"
-                file = await aiofiles.open("db/" + ctx.message.author.name + "/acheck", "r+")
-                await file.write(acheck)
-            else:
-                await bot.send_message(ctx.message.channel, dersargumani + args[0] + " adında bir soru bulunamadı" + ctx.message.author.mention)                                         
+    file = await aiofiles.open("db/" + ctx.message.author.name + "/ders", "r")
+    dersargumani = await file.read()
+    path = "db/" + ctx.message.author.name + "/Çözülmemiş/" + dersargumani
+    if dersargumani != "NULL" and len(args) == 1 and os.path.exists(path):
+        if os.path.isfile(path + "/" + dersargumani + args[0] + ".jpg"):              
+            await bot.send_file(ctx.message.channel, path + "/" + dersargumani + args[0] + ".jpg")
+            await bot.send_message(ctx.message.channel, "bu soru için cevap gönderin, iptal için iptal yazın." + ctx.message.author.mention)
+            acheck = dersargumani + args[0] + ".jpg"
+            file = await aiofiles.open("db/" + ctx.message.author.name + "/acheck", "r+")
+            await file.write(acheck)
+        else:
+            await bot.send_message(ctx.message.channel, dersargumani + args[0] + " adında bir soru bulunamadı" + ctx.message.author.mention)                                         
     return
 
 @bot.command(pass_context=True)
 async def d(ctx, *args):
-    if kayit_check(ctx.message) == True:
-        global dcheck
-        if len(args) == 0 and dcheck == 0:
-            dcheck = 1
-            return await bot.send_message(ctx.message.channel, "```\nGeometri = 1\nMatematikTYT = 2\nMatematikAYT = 3\nFizik = 4\nKimya = 5\nBiyoloji = 6\nTürkçe = 7\nCoğrafya = 8\nFelsefe = 9\nTarih = 10\nDin = 11\n```")
+    
+    global dcheck
+    if len(args) == 0 and dcheck == 0:
+        dcheck = 1
+        return await bot.send_message(ctx.message.channel, "```\nGeometri = 1\nMatematikTYT = 2\nMatematikAYT = 3\nFizik = 4\nKimya = 5\nBiyoloji = 6\nTürkçe = 7\nCoğrafya = 8\nFelsefe = 9\nTarih = 10\nDin = 11\n```")
             
-        if len(ctx.message.attachments) > 0:
-            print("bu ifadeyle aynı anda soru eklenemez.")
-        if len(args) == 1:
-            if args[0] == "l":
-                return await bot.send_message(ctx.message.channel, "```\nGeometri = 1\nMatematikTYT = 2\nMatematikAYT = 3\nFizik = 4\nKimya = 5\nBiyoloji = 6\nTürkçe = 7\nCoğrafya = 8\nFelsefe = 9\nTarih = 10\nDin = 11\n```")
-            elif args[0] in {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'}:
-                dcheck = 0
-                return await selectDers(ctx.message)
-            else:
-                print("geçersiz ders adı")
-                return await bot.send_message(ctx.message.channel, "geçersiz ders adı. Lütfen geçerli bir ders adı giriniz. yardım için /yardim " + ctx.message.author.mention)
+    if len(ctx.message.attachments) > 0:
+        print("bu ifadeyle aynı anda soru eklenemez.")
+    if len(args) == 1:
+        if args[0] == "l":
+            return await bot.send_message(ctx.message.channel, "```\nGeometri = 1\nMatematikTYT = 2\nMatematikAYT = 3\nFizik = 4\nKimya = 5\nBiyoloji = 6\nTürkçe = 7\nCoğrafya = 8\nFelsefe = 9\nTarih = 10\nDin = 11\n```")
+        elif args[0] in {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'}:
+            dcheck = 0
+            return await selectDers(ctx.message)
         else:
-            return await bot.send_message(ctx.message.channel, "Lütfen tek bir argüman giriniz. " + ctx.message.author.mention)
+            print("geçersiz ders adı")
+            return await bot.send_message(ctx.message.channel, "geçersiz ders adı. Lütfen geçerli bir ders adı giriniz. yardım için /yardim " + ctx.message.author.mention)
     else:
-        return await bot.send_message(ctx.message.channel, "Bu eylemi gerçekleştirmeniz için kaydolmanız gerekli, kaydolmak için /r yazın. " + ctx.message.author.mention)
+        return await bot.send_message(ctx.message.channel, "Lütfen tek bir argüman giriniz. " + ctx.message.author.mention)
+    
         
 @bot.command(pass_context=True)
 async def hepsi(ctx, *args):
@@ -331,19 +324,15 @@ async def hepsi(ctx, *args):
         return await bot.send_message(ctx.message.channel, "Bu komutu kullanmanız için gereken yetkilere sahip değilsiniz." + ctx.message.author.mention)
 
 async def sudo_check(message: Message):
-    if kayit_check(message):
-        file = await aiofiles.open("db/" + message.author.name + "/sudo" , "r")
-        sudo = await file.read()
-        if sudo == "1":
-            print("Kullanıcı " + message.author.name + " sudo yetkilerine sahip, izin verildi.")
-            return True
-        else:
-            await bot.send_message(message.channel, message.author.mention + " sudo yetkilerine sahip değil, bu olay raporlanacak")
-            return False
+    
+    file = await aiofiles.open("db/" + message.author.name + "/sudo" , "r")
+    sudo = await file.read()
+    if sudo == "1":
+        print("Kullanıcı " + message.author.name + " sudo yetkilerine sahip, izin verildi.")
+        return True
     else:
-        await bot.send_message(message.channel, "Bu eylemi gerçekleştirmeniz için kaydolmanız gerekli, kaydolmak için /r yazın. " + message.author.mention)
-        return False
-        
+        await bot.send_message(message.channel, message.author.mention + " sudo yetkilerine sahip değil, bu olay raporlanacak")
+        return False        
 
 @bot.group(pass_context=True)
 async def sudo(ctx):
@@ -365,11 +354,11 @@ async def yazi(ctx, *args):
 
 @sudo.command(pass_context=True)
 async def restart(ctx):
+    
     process = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE)
     await bot.send_message(ctx.message.channel, str(process.communicate()[0], "utf-8"))
     await bot.send_message(ctx.message.channel, 'Restarting...')
     os.execl("botstart.py", sys.argv[0])
-
     
 tokenfile = open("db/token", "r")
 token = tokenfile.read()
